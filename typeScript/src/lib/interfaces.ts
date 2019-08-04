53b3f0f015261660108e20f0cc5b359a5d6aa0b4
sarpong <abasimisarpong@rocketmail.com>
2019-08-04 10:55:55 +0100

    optional

--- typeScript/src/lib/interfaces.ts
+++ typeScript/src/lib/interfaces.ts
@@ -4,9 +4,27 @@ interface NameInterface {
 
 }
 
-export const name = (userName: NameInterface )=> {
+export const name = (userName: NameInterface ) : string => {
 
   return(`${userName.firstName} ${userName.lastName}`)
 }
 
 const myObject = { firstName: 'hey', lastName: 'yes'}
+
+// Example two
+
+interface Fruits {
+  favFruit?: string;
+  leastFavFruit?: string
+}
+
+const whatIsYourFacFruit = (userInput: Fruits): { favFruit: string; leastFavFruit:string } => {
+  let FruitObject = { favFruit: 'apple', leastFavFruit: 'banana'}
+  if(userInput.favFruit) {
+    FruitObject.favFruit = userInput.favFruit
+  }
+  if(userInput.leastFavFruit){
+    FruitObject.leastFavFruit = userInput.leastFavFruit
+  }
+  return FruitObject
+} 
