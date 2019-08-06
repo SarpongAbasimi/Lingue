fc43fc605f7e5cc341ae0b3c2acdb04a8ce55555
sarpong <abasimisarpong@rocketmail.com>
2019-08-06 16:12:16 +0100

    interface js

---
+++ typeScript/build/lib/classInterface.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Car {
+    constructor(numOfTyres) {
+        this.fuel = 'empty';
+        this.tyres = numOfTyres;
+    }
+}
+exports.Car = Car;
