a3f970b0b3eb50250ab597fad5e87d39898c4324
sarpong <abasimisarpong@rocketmail.com>
2019-08-06 16:42:44 +0100

    removed transpiled ts

--- typeScript/build/lib/classInterface.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Car {
-    constructor(numOfTyres) {
-        this.fuel = 'empty';
-        this.tyres = numOfTyres;
-    }
-}
-exports.Car = Car;
--- typeScript/build/test/hello.test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const hello_1 = require("../lib/hello");
-describe('hello', () => {
-    test('that is returns hello', () => {
-        expect(hello_1.hello()).toBe('hello');
-    });
-});
