7843ed7d72acf1a8759bbb7132dd7b4041aa16f7
sarpong <abasimisarpong@rocketmail.com>
2019-08-06 16:11:27 +0100

    using interface

--- typeScript/build/lib/interfaces.js
+++ typeScript/build/lib/interfaces.js
@@ -4,3 +4,13 @@ exports.name = (userName) => {
     return (`${userName.firstName} ${userName.lastName}`);
 };
 const myObject = { firstName: 'hey', lastName: 'yes' };
+const whatIsYourFacFruit = (userInput) => {
+    let FruitObject = { favFruit: 'apple', leastFavFruit: 'banana' };
+    if (userInput.favFruit) {
+        FruitObject.favFruit = userInput.favFruit;
+    }
+    if (userInput.leastFavFruit) {
+        FruitObject.leastFavFruit = userInput.leastFavFruit;
+    }
+    return FruitObject;
+};
