d643dbe73a59377c23a357f3d2942c693d29c893
sarpong <abasimisarpong@rocketmail.com>
2019-08-07 19:41:54 +0100

    added form to index page and wrote test for it

--- db/test/app.test.js
+++ db/test/app.test.js
@@ -1,12 +1,23 @@
 const app = require('../server'),
 request = require('supertest');
 
-describe('Get/', ()=> {
+describe('GET/', ()=> {
   it('must say hello', (done)=> {
     request(app)
     .get('/')
     .expect('Content-Type', /html/)
     .expect(200)
-    .expect(/Hey/, done)
+    .expect(/Welcome to the home page/, done);
+  });
+});
+
+describe('Post', ()=> {
+  it('can post data to a route', (done)=>{
+    request(app)
+    .post('/')
+    .send({ name: 'chris'})
+    .expect(200,{
+      name: 'chris'
+    }, done)
   });
 });
