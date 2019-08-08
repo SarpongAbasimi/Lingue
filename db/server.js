08e370ca6f5b7ba02fb1694ea1ddc8a671594854
sarpong <abasimisarpong@rocketmail.com>
2019-08-08 18:18:06 +0100

    server

--- db/server.js
+++ db/server.js
@@ -1,6 +1,7 @@
 const express = require('express'),
 path = require('path'),
 bodyParser = require('body-parser'),
+mongoose = require('mongoose'),
 process = require('process');
 
 require('dotenv').config()
@@ -11,6 +12,21 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'))
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use('/', require('./routes/index'));
+
+// Database connection
+mongoose.connect(process.env.MONGO_DB_URI, {useNewUrlParser: true}).catch(connectionError =>{
+  console.log(connectionError);
+});
+
+// let dbConnection = mongoose.connection;
+
+// dbConnection.on('error', (err)=> {
+//   process.stdout.write(`db connection error: ${err}`)
+// });
+
+// dbConnection.once('open', ()=>{
+// console.log('db has been successfully connected')
+// });
 
 const PORT = process.env.PORT || 3000
 
