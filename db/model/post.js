fcb03d039f915a9ae2bff28c3ab15e756aa161aa
sarpong <abasimisarpong@rocketmail.com>
2019-08-08 16:51:34 +0100

    created the post model

---
+++ db/model/post.js
@@ -0,0 +1,14 @@
+let mongoose = require('mongoose'),
+process = require('process')
+
+// mongoose.connect(process.env.MONGO_DB_URI, {useNewUrlParser: true});
+const PostShema = new mongoose.Schema({
+  post:{
+    type: String,
+    required: true
+  }
+});
+
+let Post = new mongoose.model('Post', PostShema);
+
+module.exports = Post
