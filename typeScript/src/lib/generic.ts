2337eff495dc03abb8d5a69a434179c630b7ceda
sarpong <abasimisarpong@rocketmail.com>
2019-08-14 14:57:56 +0100

    specify return value of a generic

--- typeScript/src/lib/generic.ts
+++ typeScript/src/lib/generic.ts
@@ -1,4 +1,8 @@
 
 export const generic = <T> (userInput: T) : T => {
   return userInput
-}
+};
+
+export const genericType = <T> (userInput: T[]): T[] => {
+  return userInput
+}; 
--- typeScript/src/test/generic.test.ts
+++ typeScript/src/test/generic.test.ts
@@ -1,7 +1,11 @@
-import { generic } from '../lib/generic';
+import { generic, genericType } from '../lib/generic';
 
 describe('Generic Function', ()=> {
   test('that when passed an argument it returns that argument', ()=>{
     expect(generic('hey')).toBe('hey')
+  });
+
+  test('that you can specify type of generic', ()=> {
+    expect(genericType([])).toBeInstanceOf(Array)
   });
 });
