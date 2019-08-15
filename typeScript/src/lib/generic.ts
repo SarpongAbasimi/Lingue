1cdae8cf724e830b6e654d6d9ce4804f264848ee
sarpong <abasimisarpong@rocketmail.com>
2019-08-15 17:18:55 +0100

    generics

--- typeScript/src/lib/generic.ts
+++ typeScript/src/lib/generic.ts
@@ -5,4 +5,10 @@ export const generic = <T> (userInput: T) : T => {
 
 export const genericType = <T> (userInput: T[]): T[] => {
   return userInput
-}; 
+};
+
+
+export const arrayLenght = <T>(params: T[]): T[] => {
+  console.log(params.length)
+  return params
+}
--- typeScript/src/test/generic.test.ts
+++ typeScript/src/test/generic.test.ts
@@ -1,11 +1,20 @@
-import { generic, genericType } from '../lib/generic';
+import { generic, genericType, arrayLenght } from '../lib/generic';
 
 describe('Generic Function', ()=> {
   test('that when passed an argument it returns that argument', ()=>{
-    expect(generic('hey')).toBe('hey')
+    expect(generic('hey')).toBe('hey');
   });
 
   test('that you can specify type of generic', ()=> {
-    expect(genericType([])).toBeInstanceOf(Array)
+    expect(genericType([])).toBeInstanceOf(Array);
   });
+
+  test('that array properties can be available on generis', ()=> {
+    const mySpy = jest.spyOn(console, 'log');
+    let myArray = [1,2,3,4,5];
+    expect(arrayLenght(myArray)).toBe(myArray)
+    expect(mySpy).toHaveBeenCalled()
+    expect(mySpy).toHaveBeenCalledWith(5)
+  });
+  
 });
