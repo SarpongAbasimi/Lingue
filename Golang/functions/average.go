a82575fe9887588797c216b635f91c1ddbe65bdc
sarpong <abasimisarpong@rocketmail.com>
2019-08-24 18:51:21 +0100

    second average

--- Golang/functions/average.go
+++ Golang/functions/average.go
@@ -10,6 +10,14 @@ func average(scoreList []float64) float64 {
 	return total
 }
 
+func secondAverage(scoreList []float64) float64 {
+	total := 0.0
+	for i := 0; i < len(scoreList); i++ {
+		total += scoreList[i]
+	}
+	return total
+}
+
 func main() {
 	inputSlice := []float64{2, 4, 5, 6}
 	fmt.Println(average(inputSlice))
