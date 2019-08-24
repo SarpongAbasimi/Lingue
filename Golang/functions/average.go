4a6e93b5df511fa38eb4ecb918f8b53be6bfea48
sarpong <abasimisarpong@rocketmail.com>
2019-08-24 18:42:48 +0100

    average function

--- Golang/functions/average.go
+++ Golang/functions/average.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+func average(scoreList []float64) float64 {
+	total := 0.0
+	for _, elementsInScoreList := range scoreList {
+		total += elementsInScoreList
+	}
+	return total
+}
+
 func main() {
-	fmt.Println("Yes I am working")
+	inputSlice := []float64{2, 4, 5, 6}
+	fmt.Println(average(inputSlice))
 }
