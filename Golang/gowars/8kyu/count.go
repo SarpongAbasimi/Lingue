0b642d8015f0cd0163f5aac2f648b0f08a49fe32
sarpong <abasimisarpong@rocketmail.com>
2019-08-26 19:19:46 +0100

    mumbling

--- Golang/gowars/8kyu/count.go
+++ Golang/gowars/8kyu/count.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func mapss(x []int) []int {
 	userInput := x
 	answerContainer := make([]int, len(userInput))
@@ -22,13 +20,5 @@ func monkeyCount(n int) []int {
 }
 
 func main() {
-	arrayOfInt := []int{
-		2,
-		4,
-		5,
-		6,
-	}
-	fmt.Println(mapss(arrayOfInt))
-	fmt.Println(monkeyCount(1))
 
 }
