c6e0508db228f292ebbb6048688077ea41fcc339
sarpong <abasimisarpong@rocketmail.com>
2019-08-27 15:38:59 +0100

    created a bank class

---
+++ Java/Bank.java
@@ -0,0 +1,20 @@
+public class Bank{
+  double deposit;
+  String accontOwnerName;
+  double balance;
+
+  public Bank(double initialDeposit, String name){
+    deposit = initialDeposit;
+    accontOwnerName = name;
+  }
+
+  public void updateBalance(double amount){
+    balance += amount;
+    System.out.println("Your new balance is " + "£" + balance);
+  }
+
+ public static void main(String[] args) {
+   Bank newBank = new Bank(0, "chris");
+   newBank.updateBalance(20);
+  }
+}
