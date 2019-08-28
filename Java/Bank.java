8a587d5db519a16760d89e45cc92342c3c01ccc0
sarpong <abasimisarpong@rocketmail.com>
2019-08-28 12:55:39 +0100

    using if-then

--- Java/Bank.java
+++ Java/Bank.java
@@ -14,12 +14,19 @@ public class Bank{
   }
 
   public void withdraw(double amount){
-    balance -= amount;
-    System.out.println("Your new balance is " + "£" + balance);
+    if(balance > amount){
+      balance -= amount;
+      System.out.println("Your new balance is " + "£" + balance);
+    }else {
+      System.out.println("You do not have enough funds to make this transaction");
+      System.out.println("You current balance is " + "£" + balance);
+      System.out.println("You can only withdraw bwtween £0 and " + "£" + balance);
+    }
   }
 
  public static void main(String[] args) {
    Bank newBank = new Bank(0, "chris");
-   newBank.updateBalance(20);
+  //  newBank.updateBalance(20);
+   newBank.withdraw(70);
   }
 }
