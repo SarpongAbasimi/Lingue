b5dd5e9977230e5580d9664b3951ac44a5de9f72
sarpong <abasimisarpong@rocketmail.com>
2019-08-31 19:02:28 +0100

    codejava

---
+++ Java/codeJava/Reverse.java
@@ -0,0 +1,20 @@
+
+import java.util.Arrays;
+
+public class Reverse{
+
+  public Reverse(){
+
+  }
+
+  public String[] revese(String inputWord){
+    String [] splitUserInput = inputWord.split("");
+    return splitUserInput;
+  }
+
+  public static void main(String[] args) {
+    Reverse newReverse = new Reverse();
+    String[] myCall = newReverse.revese("hello");
+    System.out.println(Arrays.toString(myCall));
+  }
+}
