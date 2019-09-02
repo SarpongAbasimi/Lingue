50e933b958f98b697469d5f50d12076fed799954
sarpong <abasimisarpong@rocketmail.com>
2019-09-02 11:24:12 +0100

    embedded structs

--- Golang/structs/structs.go
+++ Golang/structs/structs.go
@@ -10,14 +10,35 @@ type Person struct {
 	country string
 }
 
+type Animal struct {
+	name   string
+	colour string
+}
+
+type Lion struct {
+	animal Animal
+	sound  string
+}
+
+func whiteLion(lionDetails Lion) {
+	fmt.Println(lionDetails.sound)
+	fmt.Println(lionDetails.animal.name)
+	fmt.Println(lionDetails.animal.colour)
+}
+
 func main() {
-	name := Person{
-		name: "chris",
-		age:  10,
+	// name := Person{
+	// 	name: "chris",
+	// 	age:  10,
+	// }
+	// fmt.Println(name.name)
+	// var myStruct Person
+	// myStruct.age = 90
+	// myStruct.country = "China"
+	// fmt.Println(myStruct.age)
+	lion := Lion{
+		animal: Animal{name: "pam", colour: "blue"},
+		sound:  "roar",
 	}
-	fmt.Println(name.name)
-	var myStruct Person
-	myStruct.age = 90
-	myStruct.country = "China"
-	fmt.Println(myStruct.age)
+	whiteLion(lion)
 }
