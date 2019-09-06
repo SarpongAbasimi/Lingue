ab283a5f80db4ab0a10d879fb887eb122ed26890
sarpong <abasimisarpong@rocketmail.com>
2019-09-06 18:42:14 +0100

    test behaviours rather than state

--- ruby/test_app/spec/diary_spec.rb
+++ ruby/test_app/spec/diary_spec.rb
@@ -3,17 +3,12 @@ require "diary"
 RSpec.describe Diary do
   let(:diary){ described_class.new }
 
-  describe '#initialize' do
-    it "has zero entries" do
-      expect(diary.entries).to eq []
-    end
-  end
-
   describe "#add_entries" do
     it "allows users to add to entries" do 
       diary.add("Day one", "I had pizza with my mates")
       first_entry = diary.entries.first
       expect(first_entry[:title]).to eq('Day one')
+      expect(first_entry).to eq({title: 'Day one', body: 'I had pizza with my mates'})
     end
   end
 end
