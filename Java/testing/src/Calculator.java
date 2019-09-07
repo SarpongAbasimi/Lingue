9a6b3dd44f2a3e853727ae8eb0e1a5345b203ff9
sarpong <abasimisarpong@rocketmail.com>
2019-09-07 14:49:18 +0100

    calc source code

---
+++ Java/testing/src/Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+    public int add(int firstNumber, int secondNumber){
+        return firstNumber + secondNumber;
+    }
+
+    public int subtract(int firstNumber, int secondNumber){
+        return firstNumber - secondNumber;
+    }
+}
