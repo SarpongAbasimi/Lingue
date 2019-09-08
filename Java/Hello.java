7fce19e8d9587cc8697020049953582832f8a710
sarpong <abasimisarpong@rocketmail.com>
2019-09-08 15:21:19 +0100

    using array List

--- Java/Hello.java
+++ Java/Hello.java
@@ -1,11 +1,22 @@
-public class Hello{
-  public static void main(String[] args){
-    System.out.println("Hello, world");
-  }
+import java.util.ArrayList;
 
-  public static void eat(String[] args){
-    int age = 20;
-    String name = "sam";
-    System.out.println(name + age);
-  }
+public class Hello {
+    ArrayList<String> nameContainer;
+
+    public Hello(){
+        this.nameContainer = new ArrayList<String>();
+    }
+
+    public String sayHello(String userName){
+        return("Hello " + userName);
+    }
+
+    public void addToNameContainer(String name){
+        this.nameContainer.add(name);
+    }
+
+    public static void main(String[] args) {
+      Hello newHello = new Hello();
+      newHello.addToNameContainer("sarpong");
+    }
 }
--- Java/testing/src/HelloClass.java
+++ Java/testing/src/HelloClass.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+
 public class HelloClass {
+    ArrayList<String> nameContainer;
+
+    public HelloClass(){
+        this.nameContainer = new ArrayList<String>();
+    }
+
     public String sayHello(String userName){
         return("Hello " + userName);
+    }
+
+    public void addToNameContainer(String name){
+        this.nameContainer.add(name);
     }
 }
