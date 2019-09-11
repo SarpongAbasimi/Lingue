d0497d2fa267ca5076ca8eff018a295b28a2ee2c
sarpong <abasimisarpong@rocketmail.com>
2019-09-11 11:10:32 +0100

    go;

--- Golang/learnimp/main.go
+++ Golang/learnimp/main.go
@@ -8,6 +8,32 @@ cd .. in terminal.
 type in: go run learnimp/*.go
 */
 
+type currency int
+
+type employee struct {
+	name    string
+	age     int
+	country string
+}
+
+func people(user employee) string {
+	user.name = "haha"
+	fmt.Println(user.name)
+	fmt.Printf("this is the name: %s \n", user.name)
+	return user.name
+}
+
 func main() {
-	fmt.Println("version ==> ", version)
+	const (
+		USD currency = iota
+		EUR
+		GBP
+	)
+	//fmt.Println("version ==> ", version)
+	// symbols := [...]string{USD: "$"}
+	// values := [...]int{10: 9}
+	// fmt.Println(values)
+	fmt.Println("**********")
+	user := employee{name: "chris"}
+	fmt.Println(people(user))
 }
