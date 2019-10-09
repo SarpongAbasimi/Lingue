6d0242f0297c1e341e78e441e15bebe857629d4f
sarpong <abasimisarpong@rocketmail.com>
2019-10-09 19:58:04 +0100

    implemeted fizzbuzz

--- Java/RaulFizzBuzzBangPop/FizzBuzzBangPop.java
+++ Java/RaulFizzBuzzBangPop/FizzBuzzBangPop.java
@@ -1,5 +1,22 @@
 class FizzBuzzBangPop{
+
+  static int score(int i) {
+    return 699;
+  }
+
+  static String convert(int i) {
+    if(i % 15 == 0){
+      return "FizzBuzz";
+    }else if(i % 5 == 0){
+      return "Buzz";
+    }else{
+      return "Fizz";
+    }
+  }
+
   public static void main(String[] args) {
-    
+    for (int i = 0; i < 100; i++) {
+      System.out.println(i + ": " + convert(i) + " score: "+ score(i));
+    }
   }
 }
