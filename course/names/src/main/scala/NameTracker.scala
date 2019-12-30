8e3db7ffe04beeff8e71c4c93628d5e091953c07
sarpong <abasimisarpong@rocketmail.com>
2019-12-30 18:48:34 +0000

    [SA] able to store name

--- course/names/src/main/scala/NameTracker.scala
+++ course/names/src/main/scala/NameTracker.scala
@@ -1,9 +1,12 @@
-//abstract class Container {
-//  var container : List[String] = List("dsp");
-//}
-
 object Container {
   var container: List[String] = List();
+
+  def main(args: Array[String]): Unit = {
+    val nameOfPeopleIhaveMet = new NameTracker
+    nameOfPeopleIhaveMet.store("chris")
+    nameOfPeopleIhaveMet.store("sarp")
+    nameOfPeopleIhaveMet.printNames
+  }
 }
 
 class NameTracker {
@@ -14,5 +17,9 @@ class NameTracker {
 
   def getContainerLength:Int = {
     Container.container.length
+  }
+
+  def printNames: Unit = {
+    Container.container.foreach(name => println(name))
   }
 }
