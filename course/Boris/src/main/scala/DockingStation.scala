2a9ebe4e7de28f5cea57b889ad96cd6ea5f118d9
sarpong <abasimisarpong@rocketmail.com>
2020-01-01 11:35:25 +0000

    Added the ability to dock a bike

--- course/Boris/src/main/scala/DockingStation.scala
+++ course/Boris/src/main/scala/DockingStation.scala
@@ -1,5 +1,12 @@
-class DockingStation {
+import  collection.mutable.Buffer
+
+class DockingStation(val dockedStation: Buffer[Bike] = collection.mutable.Buffer[Bike]()) {
+  
   def releaseBike:Bike ={
     new Bike
+  }
+
+  def dock(bike: Bike): Unit = {
+    dockedStation.append(bike)
   }
 }
--- course/Boris/src/test/scala/DockingStationTest.scala
+++ course/Boris/src/test/scala/DockingStationTest.scala
@@ -4,7 +4,22 @@ class DockingStationTest extends org.scalatest.FunSuite with BeforeAndAfter {
 
   test("that it responds to release bike"){
     val dockingStation = new DockingStation
+    val bike = dockingStation.releaseBike
+    assert(bike.isInstanceOf[Bike])
+  }
+
+  test("that the docking station can dock a bike"){
+    val dockingStation = new DockingStation
     val bike = new Bike
-    assert(dockingStation.releaseBike == bike)
+    dockingStation.dock(bike)
+    assert(dockingStation.dockedStation.contains(bike))
+  }
+
+  test("tha the docking station can dock a bike"){
+    val dockingStation = new DockingStation
+    val bike = new Bike
+    dockingStation.dock(bike)
+
+    assert(dockingStation.dockedStation.length == 1)
   }
 }
