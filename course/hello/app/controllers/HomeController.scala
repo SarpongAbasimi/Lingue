d3b276af8a4281920fd21c0256c1da1d22c4cd39
sarpong <abasimisarpong@rocketmail.com>
2020-02-04 21:11:56 +0000

    Future

--- course/hello/app/controllers/HomeController.scala
+++ course/hello/app/controllers/HomeController.scala
@@ -5,12 +5,16 @@ import play.api._
 import play.api.mvc._
 import java.util.Date
 import java.text.SimpleDateFormat
+import scala.concurrent.Future
+import play.api.libs.ws._
+import scala.concurrent.ExecutionContext.Implicits.global
+
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject()(ws: WSClient, val controllerComponents: ControllerComponents) extends BaseController {
 
   /**
    * Create an Action to render an HTML page.
@@ -23,5 +27,19 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
     val date = new Date()
     val formatDate = new SimpleDateFormat().format(date)
     Ok(views.html.index(formatDate ))
+  }
+
+  def makeRequest() = Action.async {
+    val url: String = "https://jsonplaceholder.typicode.com/posts/1"
+    val requestData = ws.url(url).get()
+    Future.successful{
+    requestData.map {
+      response => 
+      val j = response.json
+      println(j)
+      j
+    }
+    }
+    Future.successful { Ok(views.html.data())}
   }
 }
