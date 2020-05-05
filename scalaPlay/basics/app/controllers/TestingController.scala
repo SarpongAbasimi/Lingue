354ec4647739eb88402bd62bfb26f82bac631193
sarpong <abasimisarpong@rocketmail.com>
2020-05-05 21:19:33 +0100

    Dealing with json

--- scalaPlay/basics/app/controllers/TestingController.scala
+++ scalaPlay/basics/app/controllers/TestingController.scala
@@ -2,14 +2,20 @@ package controllers
 import play.api._
 import com.google.inject.Singleton
 import javax.inject.Inject
-import play.api.libs.json.JsValue
-import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
+import play.api.libs.json.{JsValue}
+
+import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents, Request}
 
 @Singleton
 class TestingController @Inject() (val controllerComponents: ControllerComponents) extends BaseController {
 
   def fistAction : Action[AnyContent] = Action {
-    Ok("My Name is some Name")
+    implicit request: Request[AnyContent] =>
+      val requestBody : Option[JsValue] = request.body.asJson
+      val c = for {
+        b <- requestBody
+      } yield ( b \ "name").as[String]
+      Ok(s"My Name is some Name with request Body : => ${requestBody} and c is ${c}")
   }
 
   def secondAction: Action[AnyContent] = Action {
@@ -23,7 +29,8 @@ class TestingController @Inject() (val controllerComponents: ControllerComponent
 
   def fourthAction: Action[JsValue] = Action(parse.json) {
     implicit  request =>
-      Ok("Got request [" + request + "]")
+      val body = request.body
+      Ok("Got request [" + request + s"] ${body}")
   }
 
   def handlingResponse: Action[AnyContent] = Action {
