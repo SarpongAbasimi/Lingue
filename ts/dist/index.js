0e1d3aea7220cc90d114db294462b646014c030b
sarpong <abasimisarpong@rocketmail.com>
2020-05-12 08:53:32 +0100

    [SA] type and interface

--- ts/dist/index.js
+++ ts/dist/index.js
@@ -7,4 +7,30 @@ let c = {
 };
 let d = c.apple * 4;
 console.log(d);
+// objects
+let an = {
+    b: 4
+};
+const mb = {
+    c: 5
+};
+const nbv = 9;
+console.log(an.b);
+let ta = {
+    name: 'dfdfd',
+    age: 4
+};
+let ar = [1, 2, 3, 4, 5];
+var Language;
+(function (Language) {
+    Language[Language["Spanish"] = 0] = "Spanish";
+    Language[Language["Italain"] = 1] = "Italain";
+})(Language || (Language = {}));
+console.log(Language.Spanish);
+console.log(Language.Italain);
+const someFuction = (aString, aPersonType = { name: 'dvd', age: 9 }) => {
+    console.log(aString);
+    console.log(aPersonType.name);
+};
+someFuction('personName');
 //# sourceMappingURL=index.js.map
--- ts/src/index.ts
+++ ts/src/index.ts
@@ -8,3 +8,66 @@ let c = {
 let d = c.apple * 4;
 
 console.log(d);
+
+// objects
+
+let an: {b: number}= {
+  b: 4
+}
+const mb: {c: number} = {
+  c: 5
+}
+
+const nbv: number =9 
+console.log(an.b)
+
+// type alias
+
+type Age= number
+
+type Person = {
+  name: string,
+  age: Age
+}
+
+
+let ta:  Person = {
+  name: 'dfdfd',
+  age: 4
+}
+
+let ar: number[] = [1,2,3,4,5]
+
+enum Language {
+  Spanish,
+  Italain
+}
+
+console.log(Language.Spanish)
+console.log(Language.Italain)
+
+type Person_1 =  {
+  name: string,
+  age: number
+}
+
+const someFuction = (aString: string, aPersonType: Person_1 = {name: 'dvd', age: 9}): void => {
+  console.log(aString)
+  console.log(aPersonType.name)
+}
+
+someFuction('personName')
+
+type SomthingGood = {
+  <T>(a: T): String
+}
+
+abstract class Todo {
+  abstract todo(someTodo: string):string
+}
+
+class C extends Todo {
+  todo(someTodo: string){
+    return 'dfdfd'
+  }
+}
